0341d581deca2d9ed555ad2651c25255a58c0e7b
esteban.amaya.artekium <esteban.amaya@artekium.com>
2022-04-19 21:18:20 -0300

    add node version

--- src/main/store/index.ts
+++ src/main/store/index.ts
@@ -3,15 +3,12 @@ import createSagaMiddleware from 'redux-saga';
 import mainSaga from './main/mainSaga';
 import rootMainReducer from './main/mainReducer';
 
-// Set up redux dev tools only if we are in dev
-const reduxDevTools = process.env.NODE_ENV === 'development';
 const sagaMiddleware = createSagaMiddleware();
 
 const store = configureStore({
   reducer: {
     main: rootMainReducer,
   },
-  devTools: reduxDevTools,
   middleware: new MiddlewareArray().concat(sagaMiddleware),
 });
 
